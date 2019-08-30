aac0035e450d6dbc861833a330eaf7a6e8fb8625
keskin <keskin.mustafa.42@gmail.com>
2019-08-30 11:36:48 +0200

    Add legends in task 2

--- task2.py
+++ task2.py
@@ -20,6 +20,7 @@ def load_and_process_data(files_input_dir):
     predictions_file_names = [file_name for file_name in os.listdir(files_input_dir) if (not ('por_gt' in file_name) and not('lost_frames' in file_name))]
 
     predictions_list = []
+    prediction_model_names = []
 
     for idx, prediction_file_name in enumerate(predictions_file_names):
         prediction_file_path = os.path.join(files_input_dir, prediction_file_name)
@@ -29,13 +30,14 @@ def load_and_process_data(files_input_dir):
             prediction = prediction.iloc[:, 1:]
         prediction = np.array(prediction)
         predictions_list[idx] = prediction
+        prediction_model_names.append(predictions_file_names)
 
     gt_file_path = os.path.join(files_input_dir, 'por_gt.txt')
     gt = pd.read_csv(gt_file_path, header=None, sep=" ")
     gt = gt.iloc[:, 1:]
     gt = np.array(gt)
 
-    return predictions_list, gt
+    return predictions_file_names, predictions_list, gt
 
 
 def compute_model_angular_deviations(predictions, gt):
@@ -64,19 +66,20 @@ def compute_models_errors_vs_ratio(models_angular_deviations):
     return np.array(models_error_vs_ratio)
 
 
-def plot_error_vs_ratio(models_errors_vs_ratio, plot_output_dir):
-    for models_errors_vs_ratio in models_errors_vs_ratio:
-        plt.plot(range(45), models_errors_vs_ratio)
+def plot_error_vs_ratio(prediction_model_names, models_errors_vs_ratio, plot_output_dir):
+    for idx, models_errors_vs_ratio in enumerate(models_errors_vs_ratio):
+        plt.plot(range(45), models_errors_vs_ratio, label=prediction_model_names[idx])
+        plt.legend()
     plot_file_path = os.path.join(plot_output_dir, 'error_vs_ratio.png')
     plt.savefig(plot_file_path)
 
 
 def main(files_input_dir, plot_output_dir):
     os.makedirs(plot_output_dir, exist_ok=True)
-    predictions_list, gt = load_and_process_data(files_input_dir)
+    prediction_model_names, predictions_list, gt = load_and_process_data(files_input_dir)
     models_angular_deviations = compute_models_angular_deviations(predictions_list, gt)
     models_errors_vs_ratio = compute_models_errors_vs_ratio(models_angular_deviations)
-    plot_error_vs_ratio(models_errors_vs_ratio, plot_output_dir)
+    plot_error_vs_ratio(prediction_model_names, models_errors_vs_ratio, plot_output_dir)
 
 
 if __name__ == "__main__":
